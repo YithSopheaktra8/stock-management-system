12eacd7ab0c32f7dc9bacb1cbba35f317eb80ed2
Yith Sopheaktra <yithsopheaktra18@gmail.com>
2024-02-23 21:10:17 +0700

    DONE: update All Product partially

--- src/service/impl/ProductServiceImpl.java
+++ src/service/impl/ProductServiceImpl.java
@@ -4,7 +4,9 @@ import file.FileHandler;
 import modal.Product;
 import service.ProductService;
 import utils.Helper;
+import utils.RenderMenu;
 import utils.TableFormatter;
+import utils.ValidateInput;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +31,6 @@ public class ProductServiceImpl implements ProductService {
         List<Product> products = fileHandler.readListFile();
         TableFormatter.displayTable(products);
     }
-
-
     @Override
     public void editProduct() {
         Scanner scanner = new Scanner(System.in);
@@ -40,10 +40,26 @@ public class ProductServiceImpl implements ProductService {
         for (Product product : products){
             if (product.getCode().equals(code)){
                 TableFormatter.showOneProduct(product);
-                Helper.updateAll(product);
+                RenderMenu.updateMenu();
+                String userInput = ValidateInput.validateInputString("> Option [1-5] : ","! Please Input from 1-5","[1-5]+",scanner);
+                switch (userInput) {
+                    case "1" -> Helper.updateAll(product);
+                    case "2" -> {
+                        String newName = ValidateInput.validateInputString("> Please Enter new Product name : ", "! Please Input alphabet only", "[a-zA-Z\\s]+", scanner);
+                        product.setName(newName);
+                    }
+                    case "3" -> {
+                        double newPrice = Double.parseDouble(ValidateInput.validateInputString("> Please Enter new Product price : ", "! Please Input Decimal only", "[0-9]+", scanner));
+                        product.setPrice(newPrice);
+                    }
+                    case "4" -> {
+                        int newQty = Integer.parseInt(ValidateInput.validateInputString("> Please Enter new Product price : ", "! Please Input Decimal only", "[0-9]+", scanner));
+                        product.setQuantity(newQty);
+                    }
+                }
+
             }
         }
         fileHandler.writeListToFile(products);
     }
-
 }
---
+++ src/utils/RenderMenu.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class RenderMenu {
+    public static void updateMenu(){
+        System.out.println("# What do you want to update?");
+        System.out.println("1. All");
+        System.out.println("2. NAME");
+        System.out.println("3. UNIT PRICE");
+        System.out.println("4. QTY");
+        System.out.println("5. Back to Menu");
+    }
+}
